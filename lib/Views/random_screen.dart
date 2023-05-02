988610dab829ff2b6002651cbb409e90156cd83e
Miguerubsk <miguerubsk@gmail.com>
2023-05-02 12:44:25 +0200

    Update random_screen.dart

--- lib/Views/random_screen.dart
+++ lib/Views/random_screen.dart
@@ -1,12 +1,33 @@
+import 'package:barman_app/Components/cocktails_grid.dart';
+import 'package:barman_app/Models/http_cocktail_service.dart';
 import 'package:flutter/material.dart';
 
 
 class RandomCocktailScreen extends StatelessWidget {
-  const RandomCocktailScreen({Key? key}) : super(key: key);
+
+  final cocktailService = HttpCocktailService();
+
+  RandomCocktailScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Aquí apareceran 10 cocteles aleatorios'),
+    return FutureBuilder(
+      future: cocktailService.getRandomCocktails(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('No se puede conectar con el servidor'),
+            );
+          } else {
+            return Center(
+                child: CocktailGrid(cocktails: snapshot.data),
+            );
+          }
+        } else {
+          return const Center(child: CircularProgressIndicator(),);
+        }
+      },
     );
   }
 }
