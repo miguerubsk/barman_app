0eed0681b390c923719f3d8078e42db32cdc8df3
Miguerubsk <miguerubsk@gmail.com>
2023-05-02 12:44:13 +0200

    Update fav_screen.dart

--- lib/Views/fav_screen.dart
+++ lib/Views/fav_screen.dart
@@ -1,11 +1,16 @@
+import 'package:barman_app/Components/cocktails_grid.dart';
 import 'package:barman_app/Models/fav_list.dart';
 import 'package:barman_app/Views/fav_screen_empty.dart';
+import 'package:barman_app/Views/fav_screen_full.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 
 class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({Key? key}) : super(key: key);
+
+  final cocktails = FavoriteList();
+
+  FavoriteScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +22,12 @@ class FavoriteScreen extends StatelessWidget {
     return Consumer<FavoriteList>(
       builder: (context, manager, child) {
         if(manager.cocktails.isNotEmpty){
-          return FavoriteScreenEmpty(favoriteList: manager,);
+          return FavoriteScreenFull();
         } else {
           return const FavoriteScreenEmpty();
         }
       },
-    )
+    );
   }
 
 }
