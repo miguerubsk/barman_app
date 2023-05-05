0f66a022285dd89ad50cd811c95209a45acc000a
Miguerubsk <miguerubsk@gmail.com>
2023-05-05 15:59:34 +0200

    Update http_cocktail_service.dart

--- lib/Models/http_cocktail_service.dart
+++ lib/Models/http_cocktail_service.dart
@@ -16,18 +16,15 @@ class HttpCocktailService {
 
   Future<List<Cocktail>> getCocktails(List<String> url) async {
     final cocktails = <Cocktail>[];
-    try{
-      for (var search in url) {
-        final readFromServer = await _loadFromHttp('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=$search"');
-        final Map<String, dynamic> json = jsonDecode(readFromServer);
-        if (json['drinks'] != null) {
-          for (var cocktail in json['drinks']) {
-            cocktails.add(Cocktail.fromJson(cocktail));
-          }
+    for (var search in url) {
+      final readFromServer = await _loadFromHttp(
+          'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=$search"');
+      final Map<String, dynamic> json = jsonDecode(readFromServer);
+      if (json['drinks'] != null) {
+        for (var cocktail in json['drinks']) {
+          cocktails.add(Cocktail.fromJson(cocktail));
         }
       }
-    }catch (e){
-      print(e);
     }
     if (cocktails.isNotEmpty) {
       return cocktails;
@@ -38,15 +35,16 @@ class HttpCocktailService {
 
   Future<List<Cocktail>> getAllCocktails() async {
     final cocktails = <Cocktail>[];
-    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
     for (var letter in letters) {
-      final readFromServer = await _loadFromHttp("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=$letter");
+      final readFromServer = await _loadFromHttp(
+          "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=$letter");
       final Map<String, dynamic> json = jsonDecode(readFromServer);
       if (json['drinks'] != null) {
         for (var cocktail in json['drinks']) {
-          try{
+          try {
             cocktails.add(Cocktail.fromJson(cocktail));
-          }catch(e){
+          } catch (e) {
             print(e);
           }
         }
@@ -62,7 +60,8 @@ class HttpCocktailService {
   Future<List<Cocktail>> getRandomCocktails() async {
     final cocktails = <Cocktail>[];
     for (int i = 0; i < 10; i++) {
-      final readFromServer = await _loadFromHttp('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+      final readFromServer = await _loadFromHttp(
+          'https://www.thecocktaildb.com/api/json/v1/1/random.php');
       final Map<String, dynamic> json = jsonDecode(readFromServer);
       if (json['drinks'] != null) {
         for (var cocktail in json['drinks']) {
