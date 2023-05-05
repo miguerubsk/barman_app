1fccd69aa32413af4b412e8ab252b2eebafc9b14
Miguerubsk <miguerubsk@gmail.com>
2023-05-05 13:42:48 +0200

    Update search_screen.dart

--- lib/Views/search_screen.dart
+++ lib/Views/search_screen.dart
@@ -3,6 +3,7 @@ import 'package:barman_app/Models/cocktail.dart';
 import 'package:barman_app/Models/cocktail_list.dart';
 import 'package:barman_app/Models/http_cocktail_service.dart';
 import 'package:barman_app/Models/search_cocktail_delegate.dart';
+import 'package:barman_app/Views/cocktail_screen.dart';
 import 'package:flutter/material.dart';
 
 
@@ -18,19 +19,6 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: ListTile(
-          onTap: () {
-            showSearch(
-              context: context,
-              delegate: SearchCocktailDelegate(),
-            );
-          },
-          title: const Text('Search'),
-        ),
-      ),
-      body: CocktailGrid(cocktails: cocktailList.cocktails),
-    );
+    return CocktailDetailScreen(cocktail: cocktailList.cocktails.first);
   }
 }
